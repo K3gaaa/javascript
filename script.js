c6cf29c65a97b90f117e91943f8814e0804524fc
K3gaaa <danilow1@yandex.ru>
2021-09-14 17:58:05 +0700

    JSX

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const result = prompt('Как тебя зовут?');
-document.body.innerHTML = 'Привет, ' +result;
+var arr2 = [];
+for ( i = 0; i < 5; i++ ) {
+    arr2.push( Math.round( Math.random() * 100 ));
+}
+/*document.write(arr2);*/
+
+let body = document.body;
+let newElemDiv;
+
+for (elem of arr2) {
+    newEl = document.createElement('div');
+    newEl.innerHTML = elem;
+    newEl.className = 'active';
+    body.append(newEl);
+}
