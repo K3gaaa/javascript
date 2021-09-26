5a48f0f32074859820b544693159cc9b1cebef4d
K3gaaa <danilow1@yandex.ru>
2021-09-26 18:30:46 +0700

    key

--- todo1/src/components/App/App.js
+++ todo1/src/components/App/App.js
@@ -3,13 +3,19 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from "../Footer/Footer";
 
-const todoItem = ['Пройти очеоерднйо урок','убраться','поиграть'];
-const App = () => (
+const todoItem = 'Приложение';
+const App = () => {
+    const items = [
+    {value: 'Пройти очередной урок'},
+    {value: 'Убраться'},
+    {value: 'По играть'}
+];
+    return (
     <div>
         <h1>Важные дела:</h1>
         <InputItem />
-        <ItemList todoItem={todoItem} />
+        <ItemList items={items} />
         <Footer count={3} />
     </div>);
-
+}
 export default App;
