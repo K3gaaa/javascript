93976f581adced506853e9ffa33ce33d63c40e72
K3gaaa <danilow1@yandex.ru>
2021-10-12 20:28:59 +0700

    подключение css modules

--- todo1/src/components/App/App.js
+++ todo1/src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from "../Footer/Footer";
-import './App.css';
+import styles from'./App.module.css';
 
 const todoItem = 'Приложение';
 const App = () => {
@@ -12,8 +12,8 @@ const App = () => {
     {value: 'По играть'}
 ];
     return (
-    <div className="wrap">
-        <h1 className="wrap__title">Важные дела:</h1>
+    <div className={styles.wrap}>
+        <h1 className={styles.title}>Важные дела:</h1>
         <InputItem />
         <ItemList items={items} />
         <Footer count={3} />
