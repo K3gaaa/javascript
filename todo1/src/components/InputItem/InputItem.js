93119c5a43ffcdce72a8b48cac6190f29ad92ea5
K3gaaa <danilow1@yandex.ru>
2021-10-14 19:03:51 +0700

    Material UI подключение

--- todo1/src/components/InputItem/InputItem.js
+++ todo1/src/components/InputItem/InputItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<div>Входящие дела</div>);
+const InputItem = () => (<div>
+    <TextField
+        id="standard-helperText"
+        label="добавить задание"
+        variant="standard"
+    />
+</div>);
 
 export default InputItem;
