ff92b0a14b43eedf74cfaabe7b4ec14e58932d0a
K3gaaa <danilow1@yandex.ru>
2021-10-28 19:48:35 +0700

    state and Components classes

--- todo1/src/components/App/App.js
+++ todo1/src/components/App/App.js
@@ -5,21 +5,33 @@ import Footer from "../Footer/Footer";
 import styles from'./App.module.css';
 
 const todoItem = 'Приложение';
-const App = () => {
-    const items = [
-    {value: 'Пройти очередной урок',
-        isDone: true},
-    {value: 'Убраться',
-        isDone: true},
-    {value: 'По играть',
-        isDone: false}
-];
-    return (
-    <div className={styles.wrap}>
-        <h1 className={styles.title}>Важные дела:</h1>
-        <InputItem />
-        <ItemList items={items} />
-        <Footer count={3} />
-    </div>);
+class App extends React.Component {
+    state = {
+        items: [
+            {
+                value: 'Пройти очередной урок',
+                isDone: true
+            },
+            {
+                value: 'Убраться',
+                isDone: true
+            },
+            {
+                value: 'По играть',
+                isDone: false
+            }
+        ]
+    };
+
+    render() {
+            return (
+                <div className={styles.wrap}>
+                    <h1 className={styles.title}>Важные дела:</h1>
+                    <InputItem />
+                    <ItemList items={this.state.items} />
+                    <Footer count={3} />
+                </div>);
+    }
 }
+
 export default App;
